677cdf03893e15ae6374a3706022147956614361
Suchet <suchetmahamuni13@gmail.com>
2026-09-20 18:44:28 +0530

    Add allowed origin for CORS middleware

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://lifeprint-ui.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
